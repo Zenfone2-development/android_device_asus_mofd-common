67e96cb4889758c9f6057a55b16eff857f2d50c7
Arne Coucheron <arco68@gmail.com>
2016-11-04 10:51:00 +0530

    Add shim for libwvm.so
    Change-Id: Ie393d973c885f5af6e4e982bf9f1a0a1ebc84a77

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -39,3 +39,15 @@ LOCAL_MODULE := libshim_gpsd
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+#wvm
+ 
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := wvm_shim.cpp
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshim_wvm
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
